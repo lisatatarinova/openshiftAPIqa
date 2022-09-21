b211396851bc661d5e9409739d24913b1b0b9e90
lisatatarinova <lisssalavo@gmail.com>
2022-09-21 16:52:12 +0300

    kubernetes-client lib trying

---
+++ src/test/java/KubernetesClientApiDemoTest.java
@@ -0,0 +1,29 @@
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.openshift.api.model.operator.v1.OpenShiftAPIServer;
+import io.fabric8.openshift.api.model.operator.v1.OpenShiftAPIServerBuilder;
+import io.fabric8.openshift.client.NamespacedOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.fabric8.openshift.client.osgi.ManagedOpenShiftClient;
+import org.testng.annotations.Test;
+
+public class KubernetesClientApiDemoTest {
+
+    @Test
+    public void k8sClientApiDemoTest() {
+
+
+
+        //NamespacedOpenShiftClient namespacedOpenShiftClient = new ManagedOpenShiftClient().adapt(OpenShiftClient.class);
+
+        Config config = new ConfigBuilder().withMasterUrl("https://api.sandbox.x8i5.p1.openshiftapps.com:6443/")
+                .withOauthToken("sha256~Zdk-BXJrGqIeidBRlVSxSMZNwH1G3Dp9jFtQrmVZztE")
+                .build();
+        OpenShiftClient openShiftClient = new KubernetesClientBuilder().withConfig(config)
+                .build().adapt(OpenShiftClient.class);
+
+
+        openShiftClient.pods().inNamespace("lisssalavo-dev").list().getItems().stream().forEach(System.out::println);
+    }
+}
--- src/test/java/OpenshiftApiDemoTest.java
+++ src/test/java/OpenshiftApiDemoTest.java
@@ -1,18 +1,10 @@
-import com.openshift.restclient.ClientBuilder;
-import com.openshift.restclient.IClient;
-import com.openshift.restclient.ResourceKind;
-import com.openshift.restclient.model.IResource;
-import io.restassured.RestAssured;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.basic;
 
 public class OpenshiftApiDemoTest {
 
     @Test
     public void openshiftApiDemoTest() {
-        IClient client = new ClientBuilder("https://console-openshift-console.apps.sandbox.x8i5.p1.openshiftapps.com/")
+       /* IClient client = new ClientBuilder("https://console-openshift-console.apps.sandbox.x8i5.p1.openshiftapps.com/")
                 .withUserName("lisssalavo@gmail.com")
                 .withPassword("1111")
                 .build();
@@ -20,6 +12,6 @@ public class OpenshiftApiDemoTest {
         client.getAuthorizationContext().setToken("sha256~s-VJijV47sq9i98jV18bOrX1WKcySK6n-bA_kK4GfB4");
 
         IResource request = client.getResourceFactory().stub(ResourceKind.PROJECT_REQUEST, "lisssalavo-dev");
-        Assert.assertEquals(request.getName(), "lisssalavo-dev");
+        Assert.assertEquals(request.getName(), "lisssalavo-dev");*/
     }
 }
